5d47b5b1cc20859bccc7f6ac93c9ae8fd8204482
shiva <shiva_05@outlook.com>
2024-07-01 22:13:33 +0530

    added

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+
+import { Button } from "./components/ui/button";
+
+
+
+function App() {
+ 
+
+ 
+
+ return (
+    <div className="px-7 text-gray-700">
+      <section className=" bg-purple-400 rounded-lg">
+  
+        <div className="flex container sm:max-w-7xl mx-auto">
+            <h1 className=" font-bold border-r border-purple-300 pr-5 mr-5  text-2xl ">Agency</h1>
+            <div className="my-auto">
+              <Button className="lg:hidden" size="lg" variant={"default"}>Click</Button>
+              <ul className=" font-bold hidden lg:flex ">
+                <li className="px-3">Home</li>
+                <li className="px-3">Services</li>
+                <li className="px-3">Product</li>
+              </ul>
+
+            </div>
+            <Button variant={"destructive"}>Get Quote</Button>
+              
+          
+
+        </div>
+    
+
+
+
+
+      </section>
+  
+
+      
+    </div>
+  )
+}
+
+export default App
